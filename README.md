1e5278cc82f037a2ce72b633676f5e4b3a9f01be
VayerMaking <martinvayer2003@gmail.com>
2020-08-10 01:53:44 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # instafollowerbot
+## a bot for gathering more followers on instagram
+
+#ToDo
+1. da sledva followeri na daden chovek
+2. da sledva tezi koito toi sledva
+3. da trursi po hashtagove i da follow-va
+4. da commentira???
+5. da pishe v dm's
---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import urllib.request
+import os
+import time
+from instabot import Bot
+from datetime import datetime
+import http.client
+import sys
+import config
+
+
+bot = Bot()
+bot.login(username = config.username, password = config.password)
+'''
+def send( message ):
+
+    # your webhook URL
+    webhookurl = config.webhookurl
+
+    # compile the form data (BOUNDARY can be anything)
+    formdata = "------:::BOUNDARY:::\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n" + message + "\r\n------:::BOUNDARY:::--"
+
+    # get the connection and make the request
+    connection = http.client.HTTPSConnection("discordapp.com")
+    connection.request("POST", webhookurl, formdata, {
+        'content-type': "multipart/form-data; boundary=----:::BOUNDARY:::",
+        'cache-control': "no-cache",
+        })
+
+    # get the response
+    response = connection.getresponse()
+    result = response.read()
+
+    # return back to the calling function with the result
+    return result.decode("utf-8")
+'''
+
+try:
+    #bot.upload_photo("myimg.jpg",caption)
+    bot.follow_followers(username)
+    #follow-va vs hora koito dadeniqt username sledva
+    bot.follow_following(username)
+    #follow-va vs hora koito sledvat dadeniqt username
+    #print( send( "<@&693878676785463297>" + " a new post has been uploaded to instagram via your script") )
+except:
+    pass
