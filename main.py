46cf79e347092f5e2fcfe022ce5e778ff68357f4
VayerMaking <martin.vayer@gmail.com>
2020-08-17 15:46:09 +0300

    starter getting data from form

--- main.py
+++ main.py
@@ -27,7 +27,21 @@ def signup():
     #print("The email address is '" + email + "'")
     return redirect('/')
 
+@app.route('/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
+def form_example():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        language = request.form.get('language')
+        framework = request.form['framework']
 
+        return '''<h1>The language value is: {}</h1>
+                  <h1>The framework value is: {}</h1>'''.format(language, framework)
+
+    return '''<form method="POST">
+                  Language: <input type="text" name="language"><br>
+                  Framework: <input type="text" name="framework"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+              
 bot = Bot()
 bot.login(username = os.environ['config.username'], password = os.environ['config.password'])
 '''
