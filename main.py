608844bcce2c989062348d5cb53bb51bb22d5b21
VayerMaking <martin.vayer@gmail.com>
2020-08-20 01:14:09 +0300

    changed host from heroku to google compute engine

--- main.py
+++ main.py
@@ -17,30 +17,6 @@ from flask import Flask, Response
 
 uname = "qwerty"
 
-class ThreadingExample(object):
-    """ Threading example class
-    The run() method will be started and it will run in the background
-    until the application exits.
-    """
-
-    def __init__(self, interval=1):
-        """ Constructor
-        :type interval: int
-        :param interval: Check interval, in seconds
-        """
-        self.interval = interval
-
-        thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True                            # Daemonize thread
-        thread.start()                                  # Start the execution
-
-    def run(self):
-        """ Method that runs forever """
-        #while True:
-            # Do something
-            #print('Doing something imporant in the background')
-        follow(uname)
-        time.sleep(self.interval)
 
 
 app = Flask(__name__)
@@ -57,6 +33,33 @@ def signup():
     username = request.form['username']
     uname = username
     print(uname)
+    #############
+    class ThreadingExample(object):
+        """ Threading example class
+        The run() method will be started and it will run in the background
+        until the application exits.
+        """
+
+        def __init__(self, interval=1):
+            """ Constructor
+            :type interval: int
+            :param interval: Check interval, in seconds
+            """
+            self.interval = interval
+
+            thread = threading.Thread(target=self.run, args=())
+            thread.daemon = True                            # Daemonize thread
+            thread.start()                                  # Start the execution
+
+        def run(self):
+            """ Method that runs forever """
+            #while True:
+                # Do something
+                #print('Doing something imporant in the background')
+            follow(uname)
+            time.sleep(self.interval)
+
+    #############
     #follow(uname)
     example = ThreadingExample()
 
