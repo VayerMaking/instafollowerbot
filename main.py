61d4c44a15d0949985ef40fd897541f8cde05570
VayerMaking <martin.vayer@gmail.com>
2020-08-20 12:11:47 +0300

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+import json
 import urllib.request
 import os
 import threading
@@ -95,55 +96,11 @@ def send( message ):
 '''
 
 def followers (uname):
-    # instagram URL
-    URL = "https://www.instagram.com/{}/"
+    URL = 'https://www.instagram.com/linaestadeviaje/'
+    page = requests.get(URL)
 
-    # parse function
-    def parse_data(s):
-
-        # creating a dictionary
-        data = {}
-
-        # splittting the content
-        # then taking the first part
-        s = s.split("-")[0]
-
-        # again splitting the content
-        s = s.split(" ")
-
-        # assigning the values
-        data['Followers'] = s[0]
-        data['Following'] = s[2]
-        data['Posts'] = s[4]
-
-        # returning the dictionary
-        return data
-
-    # scrape function
-    def scrape_data(username):
-
-        # getting the request from url
-        r = requests.get(URL.format(username))
-
-        # converting the text
-        s = BeautifulSoup(r.text, "html.parser")
-
-        # finding meta info
-        meta = s.find("meta", property ="og:description")
-
-        # calling parse method
-        return parse_data(meta.attrs['content'])
-
-
-
-        # user name
-    username = uname
-
-        # calling scrape function
-    data = scrape_data(username)
-
-        # printing the info
-    print(data)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    followers = re.search('"edge_followed_by":{"count":(\d*)}', soup.prettify()).group(1)
 
 
 def follow(uname):
